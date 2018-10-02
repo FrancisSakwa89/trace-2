90d2018c38ef10112a7b64238df1a85cc349c650
sakwafrancis89@gmail.com <sakwafrancis89@gmail.com>
2018-10-02 10:17:02 +0300

    add scripts.js,jQuery.js and bootstrap.js classes

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+// $(document).ready(function () {
+//
+//         $("#arrive").form-control({
+// 			showOn: "button",
+// 			buttonImage: "http://www.tembeaafrica.co.ke/wp-content/themes/twentyseventeen/datepicker.jpg",
+// 			buttonImageOnly: true,
+// 			buttonText: "Select date",
+//             dateFormat: "mm/dd/yy",
+// 			minDate: 0,
+//             onSelect: function (date) {
+//                 var date2 = $('#arrive').form-control('getDate');
+//                 date2.setDate(date2.getDate() + 1);
+//                 $('#depart').form-control('setDate', date2);
+//                 //sets minDate to arrive date + 1
+//                 $('#depart').form-control('option', 'minDate', date2);
+//
+//             }
+//         });
