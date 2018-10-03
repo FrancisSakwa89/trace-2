41e26e9688a41a6002f6dab9636d8420db3b6fb7
sakwafrancis89@gmail.com <sakwafrancis89@gmail.com>
2018-10-03 14:40:10 +0300

    add the changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,41 @@
-// function sendNow(name,email) {
-//   this.firstName = first;
-//   this.lastName = last;
+// function sendNow(name,email, travelPackage,noOfPeople, arrivalDate, departureDate) {
+//   this.name = name;
+//   this.email = email;
 //   this.travavelPackage = [];
 //   this.noOfPeople=[];
 //   this.arrivalDate=[];
 //   this.departureDate=[];
 // }
+//
+// var sendNow = function( yourName, yourEmail, travavelPackage, noOfPeople,  arrrivalDate, departureDate) {
+//   return yourName + yourEmail + travavelPackage + NoOfPeople + arrrivalDate + departureDate;
+//
+// }//End of Business logic
+//
+// //User Interface
+// $(document).ready(function(event){
+//   $("form#form-group").submit(function(event){
+//
+//   event.preventDefault();
+//   var name =document.getElementById('Name').value;
+//   var emailAddress =document.getElementById('email').value;
+//   var travavelPackage =document.getElementById('Travavel Package').value;
+//   var arrivalDate =document.getElementById('arrive').value;
+//   var departureDate =document.getElementById('depart').value;
+//
+//   var formGroup = sendNow(yourName, yourEmail, travavelPackage, noOfPeople,  arrrivalDate, departureDate)
+//   $(".form-group").hide();
+//   console.log("send");
+//   //$("#outputs").text(invitedGroup)
+//   return yourName + yourEmail + travavelPackage + NoOfPeople + arrrivalDate + departureDate;
+//   $("#submit").click(function) {
+//       $("#message").show();
+//       var message =document.getElementById('message').innerHTML
+//       var message = ["name" +"email" + "Travel Package" + "arrive" + "depart"];
+//   });
+// });
+// });
 
-var sendNow = function( yourName, yourEmail, travavelPackage, noOfPeople,  arrrivalDate, departureDate) {
-  return yourName + yourEmail + travavelPackage + NoOfPeople + arrrivalDate + departureDate;
-
-}//End of Business logic
-
-//User Interface
-$(document).ready(function(event){
-  $("form#form-group").submit(function(event){
-
-  event.preventDefault();
-  var name =document.getElementById('Name').value;
-  var emailAddress =document.getElementById('email').value;
-  var travavelPackage =document.getElementById('Travavel Package').value;
-  var arrivalDate =document.getElementById('arrive').value;
-  var departureDate =document.getElementById('depart').value;
-
-
-  var formGroup = sendNow(yourName, yourEmail, travavelPackage, noOfPeople,  arrrivalDate, departureDate)
-  $(".form-group").hide();
-  console.log("send");
-  //$("#outputs").text(invitedGroup)
-});
-});
-
-
-
-
-$(document).ready(function(){
-       $("#back-1").click(function(){
-         $(".Details1").hide();
-         $(".Details2").show();
-       });
-       $("#back-2").click(function(){
-         $(".Details3").show();
-         $(".Details2").hide();
-       });
-     });
+// function myFunction() {
+//     document.getElementById("demo").style.color = "red";
+// }
